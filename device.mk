268d466c6f87f6b5559e096690cc055cc8c144db
Alex <simutalexandru@yahoo.com>
2024-02-04 02:19:35 +0200

    device.mk: enable dynamic partitions

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 
 LOCAL_PATH := device/samsung/b4q
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
